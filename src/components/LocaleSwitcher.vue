e3364b92bbac1470f422207fd89f15dacaefe0e5
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-01-10 16:04:25 +0100

    Added language switcher

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="locale-switcher">
+    <select v-model="$i18n.locale">
+      <option :value="locale.code" v-for="locale in locales" :key="locale.code">{{locale.name}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import { getSupportedLocales } from "@/util/i18n/supported-locales"
+
+export default {
+  data: () => ({ locales: [] }),
+  created() {
+    this.locales = getSupportedLocales()
+  }
+}
+</script>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,27 +1,48 @@
 <template>
-  <div id="nav">
-    <img alt="Vue logo" src="../assets/logo-circle-sm.png" />
+  <div class="nav">
+    <div class="nav__start">
+      <img alt="Vue logo" src="../assets/logo-circle-sm.png" />
 
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+      <router-link to="/">{{ $t("nav.home") }}</router-link>
+      <router-link to="/about">{{ $t("nav.about") }}</router-link>
+    </div>
+
+    <div class="nav__end">
+      <LocaleSwitcher />
+    </div>
   </div>
 </template>
 
-<style>
-#nav {
+<script>
+import LocaleSwitcher from "@/components/LocaleSwitcher"
+
+export default {
+  components: { LocaleSwitcher }
+}
+</script>
+
+<style scoped>
+.nav {
   display: flex;
+  justify-content: space-between;
   align-items: center;
   text-align: left;
   padding: 1rem;
-  color: #42b983;
+  color: #fff;
   background-color: #3d536a;
 }
 
-#nav img {
+.nav__start,
+.nav__end {
+  display: flex;
+  align-items: center;
+}
+
+.nav img {
   margin-right: 1rem;
 }
 
-#nav a {
+.nav a {
   margin-right: 1.5rem;
   font-weight: bold;
   color: #fff;
