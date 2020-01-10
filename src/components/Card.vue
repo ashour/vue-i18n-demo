d77b3a947abc21c6d11762809e7427690a24a895
Mohammad Ashour <ashour.mohammad@gmail.com>
2020-01-10 11:55:22 +0100

    Improved styling of home page and added placeholder about page content.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -22,10 +22,28 @@ export default {
 <style scoped>
 .card {
   width: 30%;
-  border: 1px solid #ccc;
+  margin-bottom: 2rem;
+  border: 1px solid #f2f2f2;
+  border-radius: 4px;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
 }
 
-.card img {
+h3 {
+  margin: 0;
+  padding: 0.5em;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+img {
   width: 100%;
+}
+
+p {
+  text-align: center;
+  margin: 0;
+  padding: 0.5em;
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Us</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nobis
+      sapiente quos officia vitae. Minus blanditiis doloremque error at
+      consectetur magni nam voluptatem pariatur! Itaque deleniti delectus odio
+      accusantium quas?
+    </p>
+
+    <p>
+      Non eius beatae dolorum impedit enim repudiandae nulla ipsum animi. Ullam
+      qui libero possimus aliquam, praesentium ipsam omnis, ab aut culpa
+      reprehenderit odio sapiente ex molestias maiores? Temporibus, molestiae
+      eum!
+    </p>
+
+    <p>
+      Consectetur illo nobis, nemo necessitatibus quos corporis voluptates enim
+      totam itaque ratione et ad iste corrupti eaque ex quas commodi.
+      Perspiciatis vel quo consectetur quas nulla explicabo harum ratione
+      consequatur.
+    </p>
   </div>
 </template>
